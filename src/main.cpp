d1cbe82e60b3d70d6f3d30e60d12c8b3a46de663
The Moon That Rises <58153205+RangerEmerald@users.noreply.github.com>
2021-10-25 19:13:09 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,6 @@
 #include "main.h"
-
-/**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
-void on_center_button() {
-	static bool pressed = false;
-	pressed = !pressed;
-	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
-	} else {
-		pros::lcd::clear_line(2);
-	}
-}
+#include "util.h"
+#include "robot.h"
 
 /**
  * Runs initialization code. This occurs as soon as the program is started.
@@ -23,10 +9,9 @@ void on_center_button() {
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
-
-	pros::lcd::register_btn1_cb(on_center_button);
+	// pros::lcd::initialize();
+	std::cout << "Initializing..." << std::endl;
+	robot::initialize();
 }
 
 /**
@@ -74,19 +59,9 @@ void autonomous() {}
  * task, not resume it from where it left off.
  */
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor left_mtr(1);
-	pros::Motor right_mtr(2);
+	std::cout << "Running opcontrol function." << std::endl;
 
 	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
-		int left = master.get_analog(ANALOG_LEFT_Y);
-		int right = master.get_analog(ANALOG_RIGHT_Y);
-
-		left_mtr = left;
-		right_mtr = right;
 		pros::delay(20);
 	}
 }
