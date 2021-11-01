f5827f0489bc41df34918a773ff09ba5342c9860
The Moon That Rises <58153205+RangerEmerald@users.noreply.github.com>
2021-11-01 17:33:07 -0600

    Added autonomous

--- include/util.h
+++ include/util.h
@@ -1,7 +1,11 @@
+#include <string>
+
 class util {
     public:
+        static std::string  brakeToString[];
         static void testWheels();
         static void testFourbar();
         static void testAll();
         static void calibrateFourbar();
+        static void autonomous();
 };
