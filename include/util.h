dbdbe3e2cf105c56fc756b7ce385d8a3f56cb364
The Moon That Rises <58153205+RangerEmerald@users.noreply.github.com>
2021-11-11 12:01:28 -0700

    Added auton helpers

--- include/util.h
+++ include/util.h
@@ -1,12 +1,16 @@
 #include <string>
+#include <vector>
 
 class util {
     public:
         static std::string brakeToString[],
-                           modeToString[];
+                           modeToString[],
+                           auton;
         static void testWheels();
         static void testFourbar();
         static void testAll();
         static bool checkOverheat();
         static void calibrateFourbar();
+        static std::string fileToAuton(std::string inputFile);
+        static std::vector<std::string> splitString(std::string str,std::string sep);
 };
--- src/util.cpp
+++ src/util.cpp
@@ -2,16 +2,19 @@
 #include "robot.h"
 #include "controller.h"
 #include <string>
+#include <vector>
+#include <fstream>
 
-std::string util::brakeToString[] = {"Coast", "Brake", "Hold"};
-std::string util::modeToString[] = {"Tank", "Single"};
+std::string util::brakeToString[] = {"Coast", "Brake", "Hold"},
+            util::modeToString[] = {"Tank", "Single"},
+            util::auton = "ls100_rs100_cT200\nls100_rs-100_cT1000";
 
 void util::testWheels() {
-    robot::moveChassis(robot::wheelNormalVelocity, robot::wheelNormalVelocity, 5000, 5000, 0);
+    robot::moveChassis(robot::wheelAddedVelocity, robot::wheelAddedVelocity, 5000, 5000, 0);
 
     pros::delay(2000);
 
-    robot::moveChassis(robot::wheelNormalVelocity, robot::wheelNormalVelocity, -5000, -5000, 0);
+    robot::moveChassis(robot::wheelAddedVelocity, robot::wheelAddedVelocity, -5000, -5000, 0);
 }
 
 void util::testFourbar() {
@@ -39,4 +42,30 @@ bool util::checkOverheat() {
 
 void util::calibrateFourbar() {
 
+}
+
+std::string util::fileToAuton(std::string inputFile) {
+    std::string commands;
+    std::string inputCommands;
+
+    std::ifstream CommandFile(inputFile);
+
+    while (std::getline(CommandFile, commands)) {
+        inputCommands += commands + "\n";
+    }
+
+    return inputCommands;
+}
+
+// Code from stack answer https://stackoverflow.com/a/16286297
+std::vector<std::string> util::splitString(std::string str,std::string sep){
+    char* cstr=const_cast<char*>(str.c_str());
+    char* current;
+    std::vector<std::string> arr;
+    current=strtok(cstr,sep.c_str());
+    while(current!=NULL){
+        arr.push_back(current);
+        current=strtok(NULL,sep.c_str());
+    }
+    return arr;
 }
