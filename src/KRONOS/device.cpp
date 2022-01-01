96c6ec0ea522267a0f3e03249bba31c8185d0dba
The Moon That Rises <58153205+RangerEmerald@users.noreply.github.com>
2022-01-01 12:36:21 -0700

    fixed compile errors

--- src/KRONOS/device.cpp
+++ src/KRONOS/device.cpp
@@ -2,6 +2,10 @@
 #include <stdexcept>
 #include <algorithm>
 
+#define S1(x) #x
+#define S2(x) S1(x)
+#define LOCATION __FILE__ " (" S2(__LINE__) ")"
+
 using namespace KRONOS;
 
 template<class D>
@@ -13,8 +17,8 @@ Device<D>::Device(D device, std::string name, std::string callsign):
     name(name),
     callsign(callsign)
 {
-    if (callsign.length() != 2) throw std::runtime_error("Callsign must be length 2. " + __FILE__ + " (" + __LINE__ + ")");
-    else if (name.length() < 3) throw std::runtime_error("Name must be length 3 or longer. " + __FILE__ + " (" + __LINE__ + ")");
+    if (callsign.length() != 2) throw std::runtime_error("Callsign must be length 2. " + std::string(LOCATION));
+    else if (name.length() < 3) throw std::runtime_error("Name must be length 3 or longer. " + std::string(LOCATION));
     
-    if (std::find(reserved.begin(), reserved.end(), callsign) != reserved.end()) throw std::runtime_err(callsign + " is a reserved sign. " + __FILE__ + " (" + __LINE__ + ")")
+    if (std::find(reserved.begin(), reserved.end(), callsign) != reserved.end()) throw std::runtime_error(callsign + " is a reserved sign. " + std::string(LOCATION));
 }
--- src/KRONOS/util.cpp
+++ src/KRONOS/util.cpp
@@ -1,15 +1,16 @@
 #include "util.hpp"
 #include <fstream>
+#include <string.h>
 
 // Code from stack answer https://stackoverflow.com/a/16286297
 std::vector<std::string> Util::splitString(const std::string& str,const std::string& sep) {
     char* cstr = const_cast<char*>(str.c_str());
     char* current;
     std::vector<std::string> arr;
-    current = std::strtok(cstr,sep.c_str());
+    current = strtok(cstr,sep.c_str());
     while(current != nullptr){
         arr.emplace_back(current);
-        current = std::strtok(nullptr,sep.c_str());
+        current = strtok(nullptr,sep.c_str());
     }
     return arr;
 }
