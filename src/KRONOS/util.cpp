f32a6c55b8113546935b6ad546be76b80b2f1494
The Moon That Rises <58153205+RangerEmerald@users.noreply.github.com>
2022-01-01 19:25:39 -0700

    update style

--- src/KRONOS/util.cpp
+++ src/KRONOS/util.cpp
@@ -5,10 +5,10 @@ std::vector<std::string> Util::splitString(const std::string& str,const std::str
     char* cstr = const_cast<char*>(str.c_str());
     char* current;
     std::vector<std::string> arr;
-    current = strtok(cstr,sep.c_str());
+    current = strtok(cstr, sep.c_str());
     while(current != nullptr){
         arr.emplace_back(current);
-        current = strtok(nullptr,sep.c_str());
+        current = strtok(nullptr, sep.c_str());
     }
     return arr;
 }
