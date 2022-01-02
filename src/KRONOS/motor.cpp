65bcd858e8a6cc90755b15cd5125a4e6ca94f3c0
The Moon That Rises <58153205+RangerEmerald@users.noreply.github.com>
2022-01-01 19:27:17 -0700

    update style

--- src/KRONOS/motor.cpp
+++ src/KRONOS/motor.cpp
@@ -2,12 +2,14 @@
 
 using namespace KRONOS;
 
-Motor::Motor(const int8_t port, const pros::motor_gearset_e_t gearset, const bool reverse, const pros::motor_encoder_units_e_t encoder, pros::motor_brake_mode_e brakemode):pros::Motor(port, gearset, reverse, encoder) {
+Motor::Motor(const int8_t port, const pros::motor_gearset_e_t gearset, const bool reverse, const pros::motor_encoder_units_e_t encoder, pros::motor_brake_mode_e brakemode):
+    pros::Motor(port, gearset, reverse, encoder)
+{
     set_brake_mode(brakemode);
 }
 
 void Motor::curve_velocity(int32_t velocity) {
-    int8_t currentVelocity = get_actual_velocity();
+    double currentVelocity = get_actual_velocity();
     double addVelocity = 5;
     double sleepTime = 2;
 
