51fc12c235fb05c757b097721d795b6b5051f887
The Moon That Rises <58153205+RangerEmerald@users.noreply.github.com>
2022-01-07 17:57:31 -0700

    gave controller more functions

--- include/KRONOS/controller.hpp
+++ include/KRONOS/controller.hpp
@@ -1,12 +1,25 @@
 #include "helpers.h"
+#include "robot.hpp"
 
 #ifndef _KRONOS_CONTROLLER_HPP_
 #define _KRONOS_CONTROLLER_HPP_
 
-class Controller: public pros::Controller {
-    public:
-        explicit Controller(pros::controller_id_e_t controller);
-        void setControllerText(const std::string& text);
-};
+namespace KRONOS {
+    class Controller: public pros::Controller {
+        private:
+            template<class T>
+            Controller& link(std::map<T, std::vector<std::string>>& linker, const T& control, const std::vector<std::string>& callsigns);
+        protected:
+            Robot& robot;
+            std::map<pros::controller_digital_e_t, std::vector<std::string>> digitalLink;
+            std::map<pros::controller_analog_e_t, std::vector<std::string>> analogLink;
+        public:
+            explicit Controller(Robot& robot);
+            void setControllerText(const std::string& text);
+            Controller& linkDigital(const pros::controller_digital_e_t& control, const std::string& callsign, const bool& isPair);
+            Controller& linkAnalog(const pros::controller_analog_e_t& control, const std::string& callsign, const bool& isPair);
+            void listener();
+    };
+}
 
 #endif
--- src/KRONOS/controller.cpp
+++ src/KRONOS/controller.cpp
@@ -1,13 +1,49 @@
 #include "controller.hpp"
 
-Controller::Controller(pros::controller_id_e_t controller):
-    pros::Controller(controller) 
+using namespace KRONOS;
+
+Controller::Controller(KRONOS::Robot& robot):
+    pros::Controller(pros::E_CONTROLLER_MASTER),
+    robot(robot)
 {
 
+}
+
+template<class T>
+Controller& Controller::link(std::map<T, std::vector<std::string>>& linker, const T& control, const std::vector<std::string>& callsigns) {
+    linker.insert({control, callsigns});
+
+    return *this;
 }
 
 void Controller::setControllerText(const std::string& text) {
     set_text(0, 0, text + "         ");
 
     std::cout << text << std::endl;
+}
+
+Controller& Controller::linkDigital(const pros::controller_digital_e_t& control, const std::string& callsign, const bool& isPair) {
+    return link(digitalLink, control, isPair ? robot.getMotorPair(callsign) : std::vector<std::string> {callsign});
+}
+
+Controller& Controller::linkAnalog(const pros::controller_analog_e_t& control, const std::string& callsign, const bool& isPair) {
+    return link(analogLink, control, std::vector<std::string> {callsign});
+}
+
+void Controller::listener() {
+    for (const auto& [controlType, devicePair] : digitalLink)
+        if (get_digital(controlType));
+
+
+    for (const auto& [controlType, devicePair] : analogLink)
+        if (get_analog(controlType))
+            for (std::string callsign: devicePair)
+                switch (robot.getDeviceType(callsign)) {
+                    case PISTON:
+                        robot.getPiston(callsign).set_value(get_analog(controlType));
+                        break;
+                    case MOTOR:
+                        robot.getMotor(callsign).move_velocity(get_analog(controlType));
+                        break;
+                }
 }
