75added95f9b1782b37e1474b605d074d58b6696
The Moon That Rises <58153205+RangerEmerald@users.noreply.github.com>
2022-02-28 20:56:58 -0700

    Fix for new claw + Remove unused functions

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,11 @@
 #include "KRONOS/helpers.h"
 
 #define YGOAL 1
+#define motorJoystickRatio 1.574
 
-KRONOS::Robot robot(0, 1);
+KRONOS::Robot robot;
 KRONOS::Controller controller(robot);
-KRONOS::Autonomous auton(robot, controller, {"fl200_fr200_bl200_br200_lf200_rf200_ln700\nfl200_fr200_bl200_br200_lf-200_rf-200_ln800\ncl\nfl-100_fr-100_bl-100_br-100_ln2400\ncl", "fl200_fr200_bl200_br200_lf200_rf200_kl200_ln700\nfl200_fr200_bl200_br200_lf-200_rf-200_kl200_ln800\ncl\nfl-200_fr-200_bl-200_br-200_kl200_lf20_rf20_ln1000\ntk_vs_0_1_px_50_110_leftTank_rightTank_80_-1\nkl-200_ln1000\nfl200_fr100_bl200_br100_ln2000", "tk_vs_0_1_px_50_110_leftTank_rightTank_30_-1", ""}, {"midGoals", "allGoals", "blueGoal", "None"}, 0);
+KRONOS::Autonomous auton(robot, controller, {"fl200_fr200_bl200_br200_lf200_rf200_ln700\nfl200_fr200_bl200_br200_lf-200_rf-200_ln800\ncl-200_ln200\nfl-100_fr-100_bl-100_br-100_ln2400\ncl200_ln200", "fl200_fr200_bl200_br200_lf200_rf200_kl200_ln700\nfl200_fr200_bl200_br200_lf-200_rf-200_kl200_ln800\ncl\nfl-200_fr-200_bl-200_br-200_kl200_lf20_rf20_ln1000\ntk_vs_0_1_px_50_110_leftTank_rightTank_80_-1\nkl-200_ln1000\nfl200_fr100_bl200_br100_ln2000", "tk_vs_0_1_px_50_110_leftTank_rightTank_30_-1", ""}, {"midGoals", "allGoals", "blueGoal", "None"}, 0);
 /**
  * Runs initialization code. This occurs as soon as the program is started.
  *
@@ -26,11 +27,10 @@ void initialize() {
 		.addMotor(KRONOS::Device(KRONOS::Motor(9, pros::E_MOTOR_GEARSET_36, true, pros::E_MOTOR_ENCODER_DEGREES, pros::E_MOTOR_BRAKE_HOLD), "rightFourbar", "rf"))
 
 		// Adding back motors
-		.addMotor(KRONOS::Device(KRONOS::Motor(11, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_DEGREES, pros::E_MOTOR_BRAKE_HOLD), "backDrag", "bd"))
 		.addMotor(KRONOS::Device(KRONOS::Motor(12, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_DEGREES, pros::E_MOTOR_BRAKE_HOLD), "backLift", "kl"))
 
-		// Adding piston claw
-		.addPiston(KRONOS::Device(KRONOS::Piston('a'), "claw", "cl"))
+		// Adding motor claw
+		.addMotor(KRONOS::Device(KRONOS::Motor(15, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_DEGREES, pros::E_MOTOR_BRAKE_HOLD), "claw", "cl"))
 
 		// Adding buttons
 		.addButton("select", pros::ADIDigitalIn('b'))
@@ -45,18 +45,8 @@ void initialize() {
 		.pairDevices({"leftFourbar", "rightFourbar"}, "fourbar"); // Pairing fourbar
 
 	controller
-		.linkAnalog(pros::E_CONTROLLER_ANALOG_RIGHT_Y, []() {
-			int leftVelocity = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y) / robot.chassisSensitivity;
-			leftVelocity += (leftVelocity != 0) ? ((leftVelocity > 0) ? robot.wheelAddVelocity : -robot.wheelAddVelocity) : 0;
-
-			robot.movePairMotors("leftTank", leftVelocity); 
-		}) // Linking left side chassis to left joystick y axis
-		.linkAnalog(pros::E_CONTROLLER_ANALOG_LEFT_Y, []() {
-			int rightVelocity = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y) / robot.chassisSensitivity;
-			rightVelocity += (rightVelocity != 0) ? ((rightVelocity > 0) ? robot.wheelAddVelocity : -robot.wheelAddVelocity) : 0;
-
-			robot.movePairMotors("rightTank", rightVelocity); 
-		}) // Linking right side chassis to right joystick y axis
+		.linkAnalog(pros::E_CONTROLLER_ANALOG_RIGHT_Y, []() { robot.movePairMotors("leftTank", controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y) * motorJoystickRatio); }) // Linking left side chassis to left joystick y axis
+		.linkAnalog(pros::E_CONTROLLER_ANALOG_LEFT_Y, []() { robot.movePairMotors("rightTank", controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y) * motorJoystickRatio); }) // Linking right side chassis to right joystick y axis
 
 		// Linking fourbar movement to left back buttons
 		.linkDigital(pros::E_CONTROLLER_DIGITAL_L1, []() { robot.movePairMotors("fourbar", 80); }, []() { robot.movePairMotors("fourbar", 0); })
@@ -65,18 +55,6 @@ void initialize() {
 		// Linking backlift movement to right back buttons
 		.linkDigital(pros::E_CONTROLLER_DIGITAL_R1, []() { robot.getMotor("backLift").move_velocity(-80); }, []() { robot.getMotor("backLift").move_velocity(0); })
 		.linkDigital(pros::E_CONTROLLER_DIGITAL_R2, []() { robot.getMotor("backLift").move_velocity(80); }, []() { robot.getMotor("backLift").move_velocity(0); })
-
-		// Linking back drag movement to up and down arrows
-		.linkDigital(pros::E_CONTROLLER_DIGITAL_UP, []() { robot.getMotor("backDrag").move_velocity(-80); }, []() { robot.getMotor("backDrag").move_velocity(0); })
-		.linkDigital(pros::E_CONTROLLER_DIGITAL_DOWN, []() { robot.getMotor("backDrag").move_velocity(80); }, []() { robot.getMotor("backDrag").move_velocity(0); })
-
-		.linkDigital(pros::E_CONTROLLER_DIGITAL_A, []() {
-			robot.chassisSensitivity = (robot.chassisSensitivity > 3) ? 1 : robot.chassisSensitivity + 0.5;
-
-			controller.setControllerText("Sensitivity < " + std::to_string(robot.chassisSensitivity));
-
-			pros::delay(100);
-		}, nullptr) // Linking controller chassis sensitivity to button a
 
 		.linkDigital(pros::E_CONTROLLER_DIGITAL_B, []() { 
 			controller.setControllerText("Resetting Fourbar");
@@ -89,18 +67,11 @@ void initialize() {
 			controller.setControllerText("Fourbar Resetted");
 		}, nullptr) // Linking fourbar reset to button b
 
-		.linkDigital(pros::E_CONTROLLER_DIGITAL_X, []() {
-			robot.wheelAddVelocity = (robot.wheelAddVelocity >= 100) ? 0 : robot.wheelAddVelocity + 50 ;
-
-			controller.setControllerText("Wheel < " + std::to_string(robot.wheelAddVelocity));
-
-			pros::delay(100);
-		}, nullptr) // Linking extra wheel speed to button x
-
 		.linkDigital(pros::E_CONTROLLER_DIGITAL_Y, []() {
-			robot.getPiston("claw").toggle();
-
-			controller.setControllerText("Front Claw < " + std::string(robot.getPiston("claw").get_value() ? "on" : "off"));
+			if (robot.getMotor("claw").get_target_position() > -150)
+				robot.getMotor("claw").move_absolute(-150, 100);
+			else
+				robot.getMotor("claw").move_absolute(0, 100);
 
 			pros::delay(200);
 		}, nullptr); // Linking claw to button y
