7490a185cb10d15fdffe440dc15143bf5ea96275
TheMoonThatRises <58153205+TheMoonThatRises@users.noreply.github.com>
2022-08-17 21:11:50 -0600

    move main header file

---
+++ KRONOS/kronos.hpp
@@ -0,0 +1,15 @@
+/*
+  @file kronos.hpp
+
+  Includes all of the KRONOS dependencies and custom functions
+*/
+
+#ifndef _KRONOS_HPP_
+#define _KRONOS_HPP_
+  /*
+    Include KRONOS libraries
+  */
+  #include "base/robot.hpp" // Main robot class
+  #include "base/devices.hpp" // Devices able to be added to the robot
+
+#endif
