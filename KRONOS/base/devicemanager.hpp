e76181d51f9a8376b1efbb9d3ef10410a51bdfa8
TheMoonThatRises <58153205+TheMoonThatRises@users.noreply.github.com>
2022-08-21 10:29:03 -0400

    add device manipulation functions

--- KRONOS/base/devicemanager.hpp
+++ KRONOS/base/devicemanager.hpp
@@ -7,10 +7,11 @@
 #ifndef _DEVICEMANAGER_HPP_
 #define _DEVICEMANAGER_HPP_
 
+#include <algorithm>
 #include <map>
 #include <vector>
 
-#include "devices.hpp"
+#include "base/devices.hpp"
 
 namespace KRONOS {
   class DeviceManager {
@@ -25,6 +26,34 @@ namespace KRONOS {
       */
       inline void set(const std::string &name, IOKParent *device) {
         devices.insert(std::make_pair(name, device));
+      }
+
+      /*
+        Gets device stored
+
+        @param name Name of sign of the device
+
+        @return The device requested
+      */
+      inline IOKParent* get(const std::string &name) {
+        return devices.at(name);
+      }
+
+      /*
+        Get all devices by a vector of keys
+
+        @param dnames Vector of keys
+
+        @return A vector of devices
+      */
+      inline std::vector<IOKParent*> valuesByKeys(const std::vector<std::string> &dnames) {
+        std::vector<IOKParent*> filtered;
+
+        for (auto [key, value] : devices)
+          if (std::find(dnames.begin(), dnames.end(), key) != dnames.end())
+            filtered.push_back(value);
+
+        return filtered;
       }
   };
 }
