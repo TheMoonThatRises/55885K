166cf60cac22781a873dadc1b26eeda018bbf0d1
TheMoonThatRises <58153205+TheMoonThatRises@users.noreply.github.com>
2022-09-09 15:19:51 -0600

    fix strafe

--- src/main.cpp
+++ src/main.cpp
@@ -30,7 +30,7 @@ void initialize() {
     .setChassisMotors(robot.getMultipleDevices({"topleft", "topright", "bottomleft", "bottomright"}))
 
     .addControllerLink({pros::E_CONTROLLER_ANALOG_LEFT_Y, pros::E_CONTROLLER_ANALOG_RIGHT_X, pros::E_CONTROLLER_ANALOG_RIGHT_Y}, [&](const std::vector<double> &analogs) {
-      robot.moveChassis(analogs[0], analogs[1], analogs[2]);
+      robot.moveChassis(analogs[0], analogs[2], analogs[1]);
     });
 
     // .addControllerLink(pros::E_CONTROLLER_ANALOG_LEFT_Y,
