d989e874bf42820ef66ce4082a57fa3c6ea6aba6
TheMoonThatRises <58153205+TheMoonThatRises@users.noreply.github.com>
2022-09-12 15:43:29 -0600

    redefine front of the robot

--- KRONOS/base/managers/chassismanager.hpp
+++ KRONOS/base/managers/chassismanager.hpp
@@ -35,7 +35,7 @@ namespace KRONOS {
       */
       inline void moveChassis(const double &straight, const double &strafe, const double &turn) {
         for (Motor *motor : _chassisMotors) {
-          std::vector<device_face> reverseStrafe {K_NORTHEAST, K_SOUTHWEST};
+          std::vector<device_face> reverseStrafe {K_NORTHWEST, K_SOUTHEAST};
 
           double mturn = motor->is_reversed() ? -turn : turn;
           double mstrafe = (std::find(reverseStrafe.begin(), reverseStrafe.end(), motor->facing()) != reverseStrafe.end()) ? strafe : -strafe;
