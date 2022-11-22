037bd2c5ec508b8128d3432378a9146a58cd1dcd
TheMoonThatRises <58153205+TheMoonThatRises@users.noreply.github.com>
2022-11-22 11:35:53 -0700

    remove unused function

--- KRONOS/assets/util.hpp
+++ KRONOS/assets/util.hpp
@@ -7,18 +7,9 @@
 #ifndef _UTIL_HPP_
 #define _UTIL_HPP_
 
-#include <chrono>
-
 namespace KUtil {
   #define MSDELAY 20.0
   #define JOYSTICK_MOTOR_RATIO 1.5748031496
-
-  /*
-    Get time since epoch in milliseconds
-  */
-  inline std::chrono::high_resolution_clock::time_point sinceEpoch() {
-    return std::chrono::high_resolution_clock::now();
-  }
 }
 
 #endif
