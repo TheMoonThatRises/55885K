0e542ab9eb450943ec42ac94f4b0810f947a76f6
TheMoonThatRises <58153205+TheMoonThatRises@users.noreply.github.com>
2022-11-23 21:10:57 -0700

    rename pid variables

--- KRONOS/base/extenders/pid.hpp
+++ KRONOS/base/extenders/pid.hpp
@@ -7,12 +7,14 @@
 #ifndef _PID_HPP_
 #define _PID_HPP_
 
+#include <cmath>
+
 namespace KPID {
   enum pid_exit_conditions {
     error, time
   };
 
-  struct pid_modifiables {
+  struct pid_consts {
     double errormargin = 0.1,
            kP = 0.5,
            kI = 0,
@@ -22,7 +24,7 @@ namespace KPID {
   class PID {
     protected:
       const pid_exit_conditions _exitcondition;
-      pid_modifiables _modifiables;
+      const pid_consts _pidconsts;
       double _previous;
 
       /*
@@ -30,13 +32,15 @@ namespace KPID {
 
         @param target Target position of the motor
         @param current Current position of the motor
+
+        @return Value to set
       */
       inline double pid(const double &target, const double &current) {
-        const double output = (_modifiables.kP * (target - current)) + (_modifiables.kD * (_previous - current));
+        const double output = (_pidconsts.kP * (target - current)) + (_pidconsts.kD * (_previous - current));
 
         _previous = current;
 
-        if (_exitcondition == error && target - output <= _modifiables.errormargin) {
+        if (_exitcondition == error && fabs(target - output) <= _pidconsts.errormargin) {
           return 0;
         } else {
           return output;
@@ -44,7 +48,11 @@ namespace KPID {
       }
 
     public:
-      inline explicit PID(const pid_exit_conditions &exitcondition, const pid_modifiables &modifiables) : _exitcondition(exitcondition), _modifiables(modifiables) {}
+      /*
+        @param exitconditions
+        @param pidconsts
+      */
+      inline explicit PID(const pid_exit_conditions &exitcondition, const pid_consts &pidconsts) : _exitcondition(exitcondition), _pidconsts(pidconsts) {}
   };
 }
 
