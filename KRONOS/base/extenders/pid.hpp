787b38bddd3044a7cd5793f7ca4eca92eb05f29e
TheMoonThatRises <58153205+TheMoonThatRises@users.noreply.github.com>
2022-11-28 13:59:42 -0700

    add logger class

--- KRONOS/base/extenders/pid.hpp
+++ KRONOS/base/extenders/pid.hpp
@@ -7,6 +7,8 @@
 #ifndef _PID_HPP_
 #define _PID_HPP_
 
+#include "pros/rtos.hpp"
+
 #include <cmath>
 
 namespace KPID {
@@ -16,12 +18,15 @@ namespace KPID {
 
   struct pid_consts {
     double errormargin = 0.1,
+           timeconstraint = 5000.0,
            kP = 0.5,
            kI = 0,
-           kD =0.4;
+           kD = 0.4;
   };
 
   class PID {
+    private:
+      double _starttime = __DBL_MIN__;
     protected:
       const pid_exit_conditions _exitcondition;
       const pid_consts _pidconsts;
@@ -36,20 +41,26 @@ namespace KPID {
         @return Value to set
       */
       inline double pid(const double &target, const double &current) {
+        if (_exitcondition == time && _starttime == __DBL_MIN__) {
+          _starttime = pros::millis();
+        }
+
         const double output = (_pidconsts.kP * (target - current)) + (_pidconsts.kD * (_previous - current));
 
         _previous = current;
 
         if (_exitcondition == error && fabs(target - output) <= _pidconsts.errormargin) {
           return 0;
+        } else if (_exitcondition == time && pros::millis() - _starttime >= 0) {
+          _starttime = __DBL_MIN__;
+          return 0;
         } else {
           return output;
         }
       }
-
     public:
       /*
-        @param exitconditions
+        @param exitcondition
         @param pidconsts
       */
       inline explicit PID(const pid_exit_conditions &exitcondition, const pid_consts &pidconsts) : _exitcondition(exitcondition), _pidconsts(pidconsts) {}
