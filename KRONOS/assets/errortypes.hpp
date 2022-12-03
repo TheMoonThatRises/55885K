7c1cd7cd16331d3a0729e25c3d748303f75c7222
TheMoonThatRises <58153205+TheMoonThatRises@users.noreply.github.com>
2022-12-03 16:59:49 -0700

    make initialiser explicit

--- KRONOS/assets/errortypes.hpp
+++ KRONOS/assets/errortypes.hpp
@@ -18,30 +18,30 @@
 namespace KRONOS {
   class AbstractKronosError : public std::runtime_error {
     public:
-      AbstractKronosError(const std::string &message) : std::runtime_error(message) {
+      explicit AbstractKronosError(const std::string &message) : std::runtime_error(message) {
         KLog::Log::error(message);
       };
   };
 
   class ColorAlreadySetError : public AbstractKronosError {
     public:
-      ColorAlreadySetError(const KUtil::side_color &previous) : AbstractKronosError("Side color already set to '" + std::to_string(previous) + "'") {};
+      explicit ColorAlreadySetError(const KUtil::side_color &previous) : AbstractKronosError("Side color already set to '" + std::to_string(previous) + "'") {};
   };
 
   class NoDeviceFoundError : public AbstractKronosError {
     public:
-      NoDeviceFoundError(const std::string &devicename) : AbstractKronosError("No device registered with name: '" + devicename + "'") {};
-      NoDeviceFoundError(const char &port) : AbstractKronosError("No device found at port: '" + std::to_string(port) + "'") {};
+      explicit NoDeviceFoundError(const std::string &devicename) : AbstractKronosError("No device registered with name: '" + devicename + "'") {};
+      explicit NoDeviceFoundError(const char &port) : AbstractKronosError("No device found at port: '" + std::to_string(port) + "'") {};
   };
 
   class PortOccupiedError : public AbstractKronosError {
     public:
-      PortOccupiedError(const char &port) : AbstractKronosError("Port is already occupied: '" + std::to_string(port) + "'") {};
+      explicit PortOccupiedError(const char &port) : AbstractKronosError("Port is already occupied: '" + std::to_string(port) + "'") {};
   };
 
   class UnexpectedDeviceFoundError : public AbstractKronosError {
     public:
-      UnexpectedDeviceFoundError(const pros::c::v5_device_e_t &expected, const device_types& got, const char& port) : AbstractKronosError("Expected device type '" + std::to_string(expected) + "' but got '" + std::to_string(got) + "'" + " at port '" + std::to_string(port) + "'") {};
+      explicit UnexpectedDeviceFoundError(const pros::c::v5_device_e_t &expected, const device_types& got, const char& port) : AbstractKronosError("Expected device type '" + std::to_string(expected) + "' but got '" + std::to_string(got) + "'" + " at port '" + std::to_string(port) + "'") {};
   };
 }
 
