ee9f5445bd5755a00755540fe0703a6a096e9982
TheMoonThatRises <58153205+TheMoonThatRises@users.noreply.github.com>
2022-12-03 17:00:42 -0700

    fix _distr initialiser + make name length const

--- KRONOS/assets/uuid.hpp
+++ KRONOS/assets/uuid.hpp
@@ -13,17 +13,18 @@ namespace KUUID {
   class UUIDGenerator {
     private:
       const std::string _characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+      const int _max_characters = 12;
 
       std::random_device _rd; // obtain a random number from hardware
       std::mt19937 _gen; // seed the generator
       std::uniform_int_distribution<> _distr; // define the range
     public:
-      explicit UUIDGenerator() : _gen(_rd()), _distr((0, _characters.size() - 1)) {};
+      explicit UUIDGenerator() : _gen(_rd()), _distr(0, (int) _characters.size() - 1) {};
 
       std::string generate_uuid() {
         std::string id;
 
-        for (int i = 0; i < 12; ++i) {
+        for (int i = 0; i < _max_characters; ++i) {
           id += _characters[_distr(_gen)];
 
           if (i % 4 == 0) {
