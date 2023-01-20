416bd2b832abed04fd1268300313f1457cd56d53
TheMoonThatRises <58153205+TheMoonThatRises@users.noreply.github.com>
2023-01-19 23:01:23 -0700

    add no exit condition + all functions public

--- KRONOS/base/extenders/pid.hpp
+++ KRONOS/base/extenders/pid.hpp
@@ -14,12 +14,13 @@
 
 namespace KPID {
   enum pid_exit_conditions {
-    P_ERROR, P_TIME
+    P_ERROR, P_NONE, P_TIME
   };
 
   struct pid_consts {
     double errormargin = 0.1,
            timeconstraint = 5000.0,
+           maxspeed = 200,
            kP = 0.5,
            kI = 0.1,
            kD = 0.4;
@@ -30,14 +31,14 @@ namespace KPID {
       std::optional<double> _starttime;
       double _previousError {}, _integral {};
 
-      const pid_exit_conditions _exitcondition;
-      const pid_consts _pidconsts;
-  protected:
-      inline void reset() {
-        _starttime.reset();
-        _previousError = 0;
-        _integral = 0;
-      }
+      pid_exit_conditions _exitcondition;
+      pid_consts _pidconsts;
+    public:
+      /*
+        @param exitcondition
+        @param pidconsts
+      */
+      inline explicit PID(const pid_exit_conditions &exitcondition, const pid_consts &pidconsts) : _exitcondition(exitcondition), _pidconsts(pidconsts) {}
 
       /*
         Get PID tuned value for target
@@ -55,7 +56,7 @@ namespace KPID {
         const double error = target - current;
         _integral += (error + _previousError) / 2;
 
-        const double output = (_pidconsts.kP * (target - current)) + (_pidconsts.kI * _integral) + (_pidconsts.kD * (error - _previousError));
+        const double output = (_pidconsts.kP * error) + (_pidconsts.kI * _integral) + (_pidconsts.kD * (error - _previousError));
 
         _previousError = error;
 
@@ -64,15 +65,23 @@ namespace KPID {
           reset();
           return 0;
         } else {
-          return output;
+          return std::min(output, _pidconsts.maxspeed);
         }
       }
-    public:
-      /*
-        @param exitcondition
-        @param pidconsts
-      */
-      inline explicit PID(const pid_exit_conditions &exitcondition, const pid_consts &pidconsts) : _exitcondition(exitcondition), _pidconsts(pidconsts) {}
+
+      inline void reset() {
+        _starttime.reset();
+        _previousError = 0;
+        _integral = 0;
+      }
+
+      inline void set_exit_condition(const pid_exit_conditions &exit) {
+        _exitcondition = exit;
+      }
+
+      inline void set_pid_consts(const pid_consts &pidconsts) {
+        _pidconsts = pidconsts;
+      }
   };
 }
 
