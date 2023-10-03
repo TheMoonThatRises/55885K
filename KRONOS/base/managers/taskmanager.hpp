1e2ce45ffc04051ea43fe9923412ed02c904eaa3
TheMoonThatRises <58153205+TheMoonThatRises@users.noreply.github.com>
2023-10-03 13:39:26 -0600

    rename `tasks` -> `_tasks` for consistency

--- KRONOS/base/managers/taskmanager.hpp
+++ KRONOS/base/managers/taskmanager.hpp
@@ -14,42 +14,49 @@
 namespace KRONOS {
   class TaskManager {
     private:
-      std::map<std::string, std::unique_ptr<pros::Task>> tasks;
+      std::map<std::string, std::unique_ptr<pros::Task>> _tasks;
     public:
       inline ~TaskManager() {
         kill_all();
       }
 
       inline void add_task(const std::string &name, const pros::Task &task) {
-        tasks.insert({ name, std::make_unique<pros::Task>(task) });
+        if (!get_task(name)) {
+          _tasks.insert({ name, std::make_unique<pros::Task>(task) });
+        }
       }
 
       inline pros::Task* get_task(const std::string& name) {
-        return tasks.find(name) != tasks.end() ? tasks.at(name).get() : nullptr;
+        return _tasks.find(name) != _tasks.end() ? _tasks.at(name).get() : nullptr;
       }
 
       inline void suspend_task(const std::string &name) {
-        if (tasks.find(name) != tasks.end()) {
-          get_task(name)->suspend();
+        pros::Task *task = get_task(name);
+
+        if (task) {
+          task->suspend();
         }
       }
 
       inline void resume_task(const std::string &name) {
-        if (tasks.find(name) != tasks.end()) {
-          get_task(name)->resume();
+        pros::Task *task = get_task(name);
+
+        if (task) {
+          task->resume();
         }
       }
 
       inline void kill_task(const std::string &name) {
-        if (tasks.find(name) != tasks.end()) {
-          tasks.at(name)->remove();
-          tasks.at(name).reset(nullptr);
-          tasks.erase(name);
+        if (get_task(name)) {
+          _tasks.at(name)->suspend();
+          _tasks.at(name)->remove();
+          _tasks.at(name).reset(nullptr);
+          _tasks.erase(name);
         }
       }
 
       inline void kill_all() {
-        for (const auto &[name, task] : tasks) {
+        for (const auto &[name, task] : _tasks) {
           kill_task(name);
         }
       }
