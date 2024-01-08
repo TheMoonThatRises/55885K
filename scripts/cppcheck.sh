e808dcc650a4e68f026dda4a102d387732e22b67
TheMoonThatRises <58153205+TheMoonThatRises@users.noreply.github.com>
2024-01-08 13:40:10 -0700

    update cppcheck options

--- scripts/cppcheck.sh
+++ scripts/cppcheck.sh
@@ -1,15 +1,1 @@
-OUTPUT=$(cppcheck --enable=all --suppressions-list=suppressions.txt -iKRONOS/external/ -IKRONOS/ src/ 2>&1)
-
-echo "$OUTPUT"
-
-TOTAL_ERRORS=0
-
-TOTAL_ERRORS=$(echo "$OUTPUT" | grep -o ": warning:" | wc -l)
-
-echo "There were $TOTAL_ERRORS errors"
-
-if (( TOTAL_ERRORS > 0 )); then
-  exit 1
-else
-  exit 0
-fi
+cppcheck --enable=all --suppressions-list=suppressions.txt --inconclusive --std=c++20 --platform=unix64 --max-ctu-depth=5 --check-level=exhaustive --error-exitcode=1 -iKRONOS/external/ -IKRONOS/ src/
