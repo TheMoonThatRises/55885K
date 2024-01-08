452b60247ee416fcef2948bfb8b7ba802685332c
TheMoonThatRises <58153205+TheMoonThatRises@users.noreply.github.com>
2024-01-08 15:39:40 -0700

    fix sleep input type

--- KRONOS/base/robot.hpp
+++ KRONOS/base/robot.hpp
@@ -355,7 +355,7 @@ class Robot
 
       @param milliseconds Length of sleep
     */
-    inline static void sleep(const int &milliseconds) {
+    inline static void sleep(const size_t &milliseconds) {
       pros::delay(milliseconds);
     }
 };
