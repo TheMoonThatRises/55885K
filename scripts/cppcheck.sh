c3bc2b8feadbbca7bb88de1c31fc2176e2f162d7
TheMoonThatRises <58153205+TheMoonThatRises@users.noreply.github.com>
2024-01-12 18:07:39 -0700

    move FreeRTOS to external/

--- scripts/cppcheck.sh
+++ scripts/cppcheck.sh
@@ -1,1 +1,1 @@
-cppcheck --enable=all --suppressions-list=suppressions.txt --inconclusive --std=c++20 --platform=unix64 --max-ctu-depth=5 --check-level=exhaustive --error-exitcode=1 -iKRONOS/external/ -IKRONOS/ src/
+cppcheck --force --enable=all --suppressions-list=suppressions.txt --inconclusive --std=c++20 --platform=unix64 --max-ctu-depth=5 --check-level=exhaustive --error-exitcode=1 -IKRONOS/ src/
